d9afcea900b79374a3a1b7e649927b0b05256f59
Dm Gn <bermitti@gmail.com>
2018-06-26 18:06:20 +0300

    complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { isArray } from "util";
+
 /* ДЗ 2 - работа с исключениями и отладчиком */
 
 /*
@@ -10,6 +12,22 @@
  Зарпещено использовать встроенные методы для работы с массивами
  */
 function isAllTrue(array, fn) {
+    if ( !Array.isArray(array) || !array.length ) {
+        // выбрасываем исключения
+        throw new Error('empty array');
+    }
+    if (typeof fn !== 'function') {
+        // выбрасываем исключения
+        throw new Error('fn is not a function');
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (!fn(array[i])) {
+            // директива return – функция завершается и значение передается обратно.
+            return false;
+        }
+    }
+
+    return true;
 }
 
 /*
@@ -22,18 +40,67 @@ function isAllTrue(array, fn) {
  Зарпещено использовать встроенные методы для работы с массивами
  */
 function isSomeTrue(array, fn) {
+    if (!Array.isArray(array) || array.length == 0) {
+        throw new Error('empty array');
+    }
+    if (typeof(fn) != 'function') {
+        throw new Error('fn is not a function')
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (fn(array[i])) {
+            return true;
+        } 
+    }
+
+    return false;
 }
 
 /*
  Задача 3:
- Функция принимает заранее неизветсное количество аргументов, первым из которых является функция fn
+ Функция принимает заранее неизвестное количество аргументов, первым из которых является функция fn
  Функция должна поочередно запусти fn для каждого переданного аргумента (кроме самой fn)
  Функция должна вернуть массив аргументов, для которых fn выбросила исключение
  Необходимо выбрасывать исключение в случаях:
  - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn) {
+function returnBadArguments(fn, ...rest) {
+    if (typeof(fn) != 'function') {
+        throw new Error('fn is not a function');
+    }
+    let result =[];
+
+    for (let i = 0; i < rest.length; i++) {
+        try {
+            // если fn выбросит исключение
+            fn(rest[i]);
+        } catch (error) {
+            // ловим исключение и добавляем в массив элемент, для которого выброшено исключение
+            result.push(rest[i]);
+        }
+    }
+
+    return result;
 }
+
+// // variant 2 with arguments
+// function returnBadArguments(fn) {
+//     if (typeof(fn) != 'function') {
+//         throw new Error('fn is not a function');
+//     }
+//     let result =[];
+
+//     for (let i = 1; i < arguments.length; i++) {
+//         try {
+//             // если fn выбросит исключение
+//             fn(arguments[i]);
+//         } catch (e) {
+//             // ловим исключение и добавляем в массив элемент, для которого выброшено исключение
+//             result.push(arguments[i]);
+//         }
+//     }
+
+//     return result;
+// }
 
 /*
  Задача 4:
@@ -49,7 +116,50 @@ function returnBadArguments(fn) {
  - number не является числом (с текстом "number is not a number")
  - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator() {
+function calculator(number = 0) {
+    // в JavaScript есть обычные числа и три специальных числовых значения: NaN, Infinity и -Infinity
+    // isFinite(n) преобразует аргумент к числу и возвращает true, если это не Infinity/-Infinity/NaN
+    // правая часть отсеет заведомо не-числа, но оставит такие значения как true/false/null и пустую строку '', так как они корректно преобразуются в числа.
+    // parseFloat преобразует аргумент к строке( true/false/null становятся "true"/"false"/"null"), а затем считывает число, при этом пустая строка даёт NaN
+    // isNaN(n) преобразует аргумент к числу и возвращает true, если получилось NaN, и false – для любого другого значения.
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+
+    if ( !isNumeric(number) ) {
+        throw new Error('number is not a number or NaN or +/-Infinity')
+    }
+
+    // возвращаем объект с методами
+    return {
+        // arr.reduce(callback[, initialValue]) обработка каждого эл. массива с сохранением промежуточного результата
+        // aргументы функции callback(previousValue, currentItem, index, arr)
+        // previousValue – последний результат вызова функции(«промежуточный результат»)
+        sum: function sum(...rest) {
+            return rest.reduce((a, b) => (a + b), number)
+        }, 
+        dif(...rest) {
+            return rest.reduce((a, b) => (a - b), number)
+        },
+        div(...rest) {
+            // проверка переданных аргументов на ноль
+            rest.forEach(function (item) {
+                if (item == 0) {
+                    throw new Error('division by 0')
+                }
+            });
+
+            // если нулей нет, то выполняем последовательное деление
+            return rest.reduce((a, b) => (a / b), number)
+            // return rest.reduce(
+            //     function (a, b) {
+            //         a / b 
+            //     }, number)
+        },
+        mul(...rest) {
+            return rest.reduce((a, b) => a * b, number)
+        }
+    }
 }
 
 export {
