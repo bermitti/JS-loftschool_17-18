7d1d575ec4a3847b6048e71ca4f62e20a77a4a13
Dm Gn <bermitti@gmail.com>
2018-07-04 23:38:15 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,109 +1,25 @@
-/* ДЗ 5 - DOM Events */
+/* ДЗ 6.1 - Асинхронность и работа с сетью */
 
 /**
- * Функция должна добавлять обработчик fn события eventName к элементу target
+ * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
  *
- * @param {string} eventName - имя события, на которое нужно добавить обработчик
- * @param {Element} target - элемент, на который нужно добавить обработчик
- * @param {function} fn - обработчик
+ * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
+ * @return {Promise}
  */
-function addListener(eventName, target, fn) {
-    target.addEventListener(eventName, fn);
+function delayPromise(seconds) {
 }
 
 /**
- * Функция должна удалять обработчик fn события eventName у элемента target
+ * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
+ * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ * Элементы полученного массива должны быть отсортированы по имени города
  *
- * @param {string} eventName - имя события, для которого нужно удалить обработчик
- * @param {Element} target - элемент, у которого нужно удалить обработчик
- * @param {function} fn - обработчик
+ * @return {Promise<Array<{name: String}>>}
  */
-function removeListener(eventName, target, fn) {
-    target.removeEventListener(eventName, fn);
+function loadAndSortTowns() {
 }
-
-/**
- * Функция должна добавлять к target обработчик события eventName, который должен отменять действие по умолчанию
- *
- * @param {string} eventName - имя события, для которого нужно удалить обработчик
- * @param {Element} target - элемент, на который нужно добавить обработчик
- */
-function skipDefault(eventName, target) {
-    target.addEventListener(eventName, e => e.preventDefault());
-}
-
-/**
- * Функция должна эмулировать событие click для элемента target
- *
- * @param {Element} target - элемент, на который нужно добавить обработчик
- */
-function emulateClick(target) {
-    target.click();
-}
-
-// # 2
-// function emulateClick(target) {
-//     let event = new CustomEvent("click");
-
-//     // выполнить для любого элемента
-//     target.dispatchEvent(event);
-// }
-
-/**
- * Функция должна добавить такой обработчик кликов к элементу target
- * который реагирует (вызывает fn) только на клики по элементам BUTTON внутри target
- *
- * @param {Element} target - элемент, на который нужно добавить обработчик
- * @param {function} fn - функция, которую нужно вызвать при клике на элемент BUTTON внутри target
- */
-function delegate(target, fn) {
-    let handler = function (e) {
-        if (e.target.tagName == 'BUTTON') {
-            fn();
-        }
-    }
-
-    target.addEventListener('click', handler);
-}
-
-/**
- * *** Со звездочкой ***
- * Функция должна добавить такой обработчик кликов к элементу target
- * который сработает только один раз и удалится
- * Постарайтесь не создавать глобальных переменных
- *
- * @param {Element} target - элемент, на который нужно добавить обработчик
- * @param {function} fn - обработчик
- */
-function once(target, fn) {
-    target.addEventListener('click', fn, { once: true });
-    // once: Boolean указывает, что слушатель должен быть вызван не более одного раза после добавления. 
-    // Если true, слушатель автоматически удаляется при вызове.
-}
-
-// #2
-// function once(target, fn) {
-//     let handler = function () {
-//         fn();
-//         target.removeEventListener('click', handler);
-//     }
-
-//     target.addEventListener('click', handler);
-// }
-
-// #3
-// function once(target, fn) {
-//     target.addEventListener("click", fn);
-//     target.addEventListener("click", () => {
-//       target.removeEventListener("click", fn);
-//     });
-//   }
 
 export {
-    addListener,
-    removeListener,
-    skipDefault,
-    emulateClick,
-    delegate,
-    once
+    delayPromise,
+    loadAndSortTowns
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,48 @@ let webpack = require('webpack');
 let HtmlPlugin = require('html-webpack-plugin');
 let CleanWebpackPlugin = require('clean-webpack-plugin');
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
-let rules = require('./webpack.config.rules')();
+let loaders = require('./webpack.config.loaders')();
 let path = require('path');
 
-rules.push({
+loaders.push({
     test: /\.css$/,
-    use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: 'css-loader'
+    loader: ExtractTextPlugin.extract({
+        fallbackLoader: 'style-loader',
+        loader: 'css-loader'
     })
 });
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js',
+        townss: './src/towns.js'
+    },
     output: {
-        filename: '[name].[hash].js',
+        filename: '[hash].js',
         path: path.resolve('dist')
     },
     devtool: 'source-map',
-    module: { rules },
+    module: {
+        loaders
+    },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             sourceMap: true,
             compress: {
-                drop_debugger: false,
-                warnings: false
+                drop_debugger: false
             }
         }),
         new ExtractTextPlugin('styles.css'),
         new HtmlPlugin({
-            title: 'Loft School sample project',
-            template: 'index.hbs'
+            title: 'Main Homework',
+            template: 'index.hbs',
+            chunks: ['main']
+        }),
+        new HtmlPlugin({
+            title: 'Div Drag And Drop',
+            template: 'towns.hbs',
+            filename: 'towns.html',
+            chunks: ['towns']
         }),
         new CleanWebpackPlugin(['dist'])
     ]
