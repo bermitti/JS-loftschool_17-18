756342ffe5026b7119307a11bc67011433cd5a96
Dm Gn <bermitti@gmail.com>
2018-07-05 11:55:30 +0300

    part-1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@
  * @return {Promise}
  */
 function delayPromise(seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            return resolve();
+        }, seconds*1000)
+    })
 }
+
+// #2
+// function delayPromise(seconds) {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(), seconds*1000);
+//     });
+// }
 
 /**
  * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
@@ -17,7 +29,43 @@ function delayPromise(seconds) {
  * @return {Promise<Array<{name: String}>>}
  */
 function loadAndSortTowns() {
+    return new Promise(function (resolve, reject) {
+        const req = new XMLHttpRequest();
+        let url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
+
+        // т.к. ответ - объект, то функция сравнения такая
+        function compare(a, b) {
+            if (a.name < b.name) { 
+                return -1; 
+            } 
+            if (a.name > b.name) { 
+                return 1; 
+            } 
+  
+            return 0;
+        }
+
+        req.open('GET', url);
+        req.onload = function () {
+            if (req.status != 200) {
+                reject('что-то не так')
+            } else {
+                // JSON.parse(str) превратит строку с данными в формате JSON в JavaScript-объект/массив/значение
+                let result = JSON.parse(req.response);
+
+                result.sort(compare);
+                resolve(result);
+            }
+        }
+        req.send();
+    })
 }
+// проверка для себя, т.к. тесты не работают
+// loadAndSortTowns()
+//   .then( (value) => {
+//     console.log(typeof(value));
+//     console.log(value);
+//   })
 
 export {
     delayPromise,
